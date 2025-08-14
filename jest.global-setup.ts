8d6cb5e79cd072f98a78fce854a065609c71a3c6
Ange230700 <kouakouangeericstephane@gmail.com>
2025-08-14 07:02:01 +0200

    chore: do stuff

--- jest.global-setup.ts
+++ jest.global-setup.ts
@@ -1,4 +1,9 @@
-import { createEphemeralDb } from "../test-utils/ephemeral-db.js";
+// user/jest.global-setup.ts
+import { createEphemeralDb } from "../test-utils/ephemeral-db.mjs";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+
 export default async () => {
   await createEphemeralDb({
     baseUrlEnvKey: "USER_DATABASE_URL",
--- jest.global-teardown.ts
+++ jest.global-teardown.ts
@@ -1,5 +1,5 @@
 // user\jest.global-teardown.ts
-import { dropEphemeralDb } from "../test-utils/ephemeral-db.js";
+import { dropEphemeralDb } from "../test-utils/ephemeral-db.mjs";
 export default async () => {
   await dropEphemeralDb(".tmp-user-test-db.json");
 };
