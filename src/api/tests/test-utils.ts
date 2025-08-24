52404aff6c04893a2bd0a4d33af29e1a60ad4c48
Ange230700 <kouakouangeericstephane@gmail.com>
2025-08-24 13:43:54 +0200

    chore: do stuff

--- src/api/tests/test-utils.ts
+++ src/api/tests/test-utils.ts
@@ -1,7 +1,7 @@
 // user/src/api/tests/test-utils.ts
 
-import { PrismaClient } from "@prisma/client";
-export const prisma = new PrismaClient();
+import { PrismaClient } from "nihildbpost/prisma/generated/client";
+export const prisma: PrismaClient = new PrismaClient();
 
 export async function cleanupTestResources() {
   await prisma.$disconnect().catch(() => {});
